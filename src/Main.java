7837fe06cf5b440f97ded57be825983a12e02708
Dmitry <d.spitsyn@internet.ru>
2022-05-24 08:01:18 +0300

    первый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Пишем дз 4!");
+        System.out.println("Переходим в коммит  анд пуш!");
+        System.out.println("Переходим в гидхаб");
     }
 }
