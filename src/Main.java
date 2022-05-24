83f0c563dc29e640e89e3fcbb7356e6e3e3263c0
Dmitry <d.spitsyn@internet.ru>
2022-05-24 08:34:53 +0300

    первый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+//Задание 1
+        int total = 0;
+        int i = 0;
+        while (total < 10) {
+        total = total + 1;
+            System.out.print(total + " ");
+        }
+        System.out.println(" ");
+    for (int n = 10; n >= 1; n=n-1) {
+            System.out.print(n + " ");
+        }
 
-        System.out.println("Пишем дз 4!");
-        System.out.println("Переходим в коммит  анд пуш!");
-        System.out.println("Переходим в гидхаб");
     }
 }
